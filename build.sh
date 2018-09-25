2356d6b81d8ca322bf346617dacd7656e227c386
wanganyang <wanganyang@bytedance.com>
2018-09-25 14:44:33 +0800

    add go install for build

--- build.sh
+++ build.sh
@@ -11,22 +11,22 @@ rm -rf output
 rm -rf ~/.m2/settings.xml
 
 # http proxy
-#export http_proxy="http://10.4.16.31:3128"
-#export https_proxy="http://10.4.16.31:3128"
+export http_proxy="http://10.4.16.31:3128"
+export https_proxy="http://10.4.16.31:3128"
 
 # golang
 
-#wget https://dl.google.com/go/go1.10.1.linux-amd64.tar.gz
+wget https://dl.google.com/go/go1.10.1.linux-amd64.tar.gz
 
-#tar -C $HOME -xzf go1.10.1.linux-amd64.tar.gz
+tar -C $HOME -xzf go1.10.1.linux-amd64.tar.gz
 
-#export PATH=$PATH:$HOME/go/bin
+export PATH=$PATH:$HOME/go/bin
 
-#export GOROOT=$HOME/go
+export GOROOT=$HOME/go
 
-#mkdir $HOME/GO_PROJECTS
+mkdir $HOME/GO_PROJECTS
 
-#export GOPATH=$HOME/GO_PROJECTS
+export GOPATH=$HOME/GO_PROJECTS
 
 dev/scripts/generate-tarballs release -debug=true -hadoop-distributions=hadoop-2.6
 
