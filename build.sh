a0aca63b4f42585dc4a310ab62f0635b07334767
wanganyang <wanganyang@bytedance.com>
2018-09-25 14:33:25 +0800

    update build.sh

--- build.sh
+++ build.sh
@@ -1,24 +1,62 @@
 #!/bin/bash
 
+#!/bin/bash
+
 set -e
 
 set -x
 
 rm -rf output
 
-mvn clean package -Phadoop-2.6 -Dhadoop.version=2.6.0-cdh5.4.4-bd3 -DskipTests -Dmaven.javadoc.skip -Dfindbugs.skip -Dcheckstyle.skip -Dlicense.skip
+rm -rf ~/.m2/settings.xml
 
-mkdir -p output
+# http proxy
+#export http_proxy="http://10.4.16.31:3128"
+#export https_proxy="http://10.4.16.31:3128"
 
-cp -R bin output/
+# golang
 
-cp -R conf output/
+#wget https://dl.google.com/go/go1.10.1.linux-amd64.tar.gz
 
-cp -R libexec output/
+#tar -C $HOME -xzf go1.10.1.linux-amd64.tar.gz
 
-cp -R integration output/
+#export PATH=$PATH:$HOME/go/bin
 
-cp -R assembly output/
+#export GOROOT=$HOME/go
 
-cp -R logs output/
+#mkdir $HOME/GO_PROJECTS
+
+#export GOPATH=$HOME/GO_PROJECTS
+
+dev/scripts/generate-tarballs release -debug=true -hadoop-distributions=hadoop-2.6
+
+mkdir output
+
+tar -zxf alluxio-*-hadoop-2.6.tar.gz
+
+cp -a -R alluxio-*-hadoop-2.6/* output
+
+
+
+#set -e
+
+#set -x
+
+#rm -rf output
+
+#mvn clean package -Phadoop-2.6 -Dhadoop.version=2.6.0-cdh5.4.4-bd3 -DskipTests -Dmaven.javadoc.skip -Dfindbugs.skip -Dcheckstyle.skip -Dlicense.skip
+
+#mkdir -p output
+
+#cp -R bin output/
+
+#cp -R conf output/
+
+#cp -R libexec output/
+
+#cp -R integration output/
+
+#cp -R assembly output/
+
+#cp -R logs output/
 
