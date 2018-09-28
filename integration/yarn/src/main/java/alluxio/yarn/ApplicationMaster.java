ac99ba56018e26a4ad35f0cea5755fa18f6ed7a8
wanganyang <wanganyang@bytedance.com>
2018-09-28 18:30:26 +0800

    debug

--- integration/yarn/src/main/java/alluxio/yarn/ApplicationMaster.java
+++ integration/yarn/src/main/java/alluxio/yarn/ApplicationMaster.java
@@ -191,33 +191,33 @@ public final class ApplicationMaster implements AMRMClientAsync.CallbackHandler 
     }
   }
 
-  private static class MessageThread extends Thread {
-    String name;
-
-    public MessageThread(String name) {
-      this.name = name;
-    }
-
-    //重写run方法
-    public void run() {
-      while (true) {
-        try {
-          Thread.sleep(10);
-        } catch (InterruptedException e) {
-          e.printStackTrace();
-        }
-        sendMessage("ApplicationMaster.main still alive");
-      }
-    }
-  }
+//  private static class MessageThread extends Thread {
+//    String name;
+//
+//    public MessageThread(String name) {
+//      this.name = name;
+//    }
+//
+//    //重写run方法
+//    public void run() {
+//      while (true) {
+//        try {
+//          Thread.sleep(10);
+//        } catch (InterruptedException e) {
+//          e.printStackTrace();
+//        }
+//        sendMessage("ApplicationMaster.main still alive");
+//      }
+//    }
+//  }
 
 
   /**
    * @param args Command line arguments to launch application master
    */
   public static void main(String[] args) throws InterruptedException {
-    MessageThread mt = new MessageThread("ApplicationMaster MessageThread");
-    mt.run();
+//    MessageThread mt = new MessageThread("ApplicationMaster MessageThread");
+//    mt.run();
     sendMessage("ApplicationMaster.main");
 //    Thread.sleep(10000000);
     Options options = new Options();
